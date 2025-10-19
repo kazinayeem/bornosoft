c25df1b5ce552f1f1cfbffcfd4155036fa79cdbf
kazinayeem <mohammadalinayeem360@gmail.com>
2025-10-19 21:10:37 +0600

    dark light add

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,27 +6,7 @@ const config: Config = {
     "./pages/**/*.{ts,tsx,js,jsx}",
     "./components/**/*.{ts,tsx,js,jsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        // Custom colors if needed
-        primary: {
-          light: "#6366F1",
-          DEFAULT: "#4F46E5",
-          dark: "#4338CA",
-        },
-        secondary: {
-          light: "#F472B6",
-          DEFAULT: "#EC4899",
-          dark: "#DB2777",
-        },
-      },
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", "ui-sans-serif", "system-ui"],
-        mono: ["var(--font-geist-mono)", "ui-monospace", "SFMono-Regular"],
-      },
-    },
-  },
+
   darkMode: "class",
   plugins: [],
 };
